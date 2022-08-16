c262c1ef78a81ee3698a2a95771dee74433bc2f6
John <johnnyplays1@gmail.com>
2022-08-15 19:26:56 -0700

    Doing some programming

---
+++ Week1/Print_a_Poem/Poem.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//Credit to Ponly.com for this poem.
+
+int main()
+{
+    printf("Roses are red, Violets aren't blue. They're violet. It's literally in the name.\n");
+    return 0;
+}
